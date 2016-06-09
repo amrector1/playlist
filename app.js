86bb4950c325dab164454776438cbd801295305b
Adam Rector <amrector1@gmail.com>
2016-06-09 09:50:01 -0600

    module2 complete

---
+++ app.js
@@ -0,0 +1,20 @@
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function(){
+  if (request.readyState === 4 && request.status < 400){
+    var object = JSON.parse(request.responseText);
+    console.log(object);
+    var array = object.results
+      for (var i = 0; i < 3; i++) {
+        var randomNum = array[Math.floor(Math.random()*array.length)];
+
+        var image = document.createElement('img');
+        image.src = "images/" + array[Math.floor(Math.random()*array.length)].cover_art;
+        var body = document.getElementById('body-main');
+        body.appendChild(image);
+
+    }
+  }
+}
+request.open("GET", "https://lit-fortress-6467.herokuapp.com/object");
+request.send();
